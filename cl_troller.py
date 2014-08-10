b1b18e65f7c1047a78be103bac9e4661c31a2d23
Ben McKown <bgmckown@gmail.com>
2014-08-09 20:56:55 -0500

    SQLite integration

--- cl_troller.py
+++ cl_troller.py
@@ -1,11 +1,26 @@
 #!/usr/bin/python
 
 from twilio.rest import TwilioRestClient
+from pprint import pprint
+import unicodedata
 import feedparser
 import sqlite3
 import email
 import time
 import sys
+import re
+
+def stringify(text):
+    return u"\'" + unicode(text) + u"\'"
+
+def insert(item):
+    pid = item["id"]
+    title = item["title"]
+    summary = item["summary"]
+    link = item["link"]
+    published = item["published"]
+    insert_statement = u"INSERT OR IGNORE INTO cl_item_list VALUES (" + u",".join(map(stringify,[pid,title,summary,link,published])) + u")"
+    return unicodedata.normalize('NFKD', insert_statement).encode('ascii','ignore')
 
 conn = sqlite3.connect("cl_list.db")
 
@@ -28,8 +43,14 @@ while True:
         sys.stderr.write("Fetching " + base_url + "\n")
         parsed = feedparser.parse(base_url)
         for item in parsed["items"]:
-            
-        time.sleep(60*10)
+            select = c.execute("SELECT * FROM cl_item_list WHERE id = ?", (item['id'],))
+            if select is None:
+                print select
+            else:
+                c.execute("INSERT INTO cl_item_list VALUES (?,?,?,?,?)" , (item['id'], item['title'], item['summary'], item['link'], item['published']))
+                print "NEW ITEM" 
+        conn.commit()
+        time.sleep(6)
     except KeyboardInterrupt as e:
         break
 
